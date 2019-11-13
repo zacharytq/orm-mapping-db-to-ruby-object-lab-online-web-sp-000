ee13e35ca466fad8b95924c8b31b8d709358f3aa
chic-page-6502 <zacharytq@gmail.com>
2019-11-13 00:55:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,17 @@ class Student
     end
   end
 
+  def self.all_students_below_grade_12
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE students.grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
