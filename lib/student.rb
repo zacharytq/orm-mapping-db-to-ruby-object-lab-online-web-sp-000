7f7eedcc9600784f717cf37f7c4aeb81be51f9de
chic-page-6502 <zacharytq@gmail.com>
2019-11-13 00:58:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
     end
   end
 
-  def self.students_below_grade_12
+  def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students
       WHERE students.grade < 12
