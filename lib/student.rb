737a42639b8081a943032fa2ad31881847239ee1
chic-page-6502 <zacharytq@gmail.com>
2019-11-13 00:59:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students
       WHERE students.grade = 10
-      LIMIT 1
+      DESC LIMIT 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
